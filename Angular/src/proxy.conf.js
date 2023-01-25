dd6295605e7aaa42be45c5d274e7026824329465
Navin V <77722071+NavinVinayagam@users.noreply.github.com>
2023-01-25 11:02:11 +0530

    Web socket connection error resolved.

--- Angular/src/proxy.conf.js
+++ Angular/src/proxy.conf.js
@@ -4,18 +4,28 @@ const target = "https://localhost:7039/"  /*  for blazor Server */
 const PROXY_CONFIG = [
   {
     context: [
+      "/weatherforecast",
       "/_content",
       "/_framework",
-      "/_blazor"
-   ],
+      "/_blazor",
+    ],
+    proxyTimeout: 3000,
     target: target,
     secure: false,
     headers: {
       Connection: 'Keep-Alive'
-    },
-    ws: true
+    }
+  },
+  {
+    context: [
+      "/_blazor"
+    ],
+    target: target,
+    secure: false,
+    ws: true,
+    logLevel: "debug"
   }
-]
+];
 
 module.exports = PROXY_CONFIG;
 
